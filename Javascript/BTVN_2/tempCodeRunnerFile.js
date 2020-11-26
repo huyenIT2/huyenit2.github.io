c70a89ea7eabb07aa1dce910ebff9389c4057632
unknown <huyenkute622001@gmail.com>
2020-11-26 21:04:39 +0700

    BTVN2

---
+++ Javascript/BTVN_2/tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+// thống kê theo tuổi
+// const statisticsByAge = listPersons.reduce(
+//         (...) => {
+//             //...
+//         }, {
+//             treCon: 0,
+//             thanhNien: 0,
+//             nguoiGia: 0
+//         }
+//     )
+//     /*
+//     output dạng: 
+//     {
+//         treCon: 5,
+//         thanhNien: 2,
+//         nguoiGia: 3
+//     }
+//     */
